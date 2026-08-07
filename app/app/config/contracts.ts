844c20e631b3cbb74744d534a89cf8ee1f4aa422
hlevi0648-wq <hlevi0648@gmail.com>
2026-08-07 13:43:18 -0600

    feat: add all deployed Sepolia contract addresses
    - OracleManager: 0xD844243646B9EB6FA6B1Bb7bDEe7CD6B1773b4cf
    - EmergencyPause: 0xFdcB2225A3D639004F60BDe28D48CF2403460b6e
    - RewardDistributor: 0x568353587bB13D90BE4A49752c7b53aab79F40b4
    - Treasury & StrategyManager: pending (need more ETH)

--- app/app/config/contracts.ts
+++ app/app/config/contracts.ts
@@ -1,11 +1,11 @@
 export const CONTRACTS = {
   verseToken: "0xC0B495539d9814dcbA869A9619E87B95881A2536",
   stakingVault: "0xe729a24765485b7F8F712F1B0BA97C6eBC125F9c",
-  rewardDistributor: "",
+  rewardDistributor: "0x568353587bB13D90BE4A49752c7b53aab79F40b4",
   treasury: "",
   strategyManager: "",
-  oracleManager: "",
-  emergencyPause: "",
+  oracleManager: "0xD844243646B9EB6FA6B1Bb7bDEe7CD6B1773b4cf",
+  emergencyPause: "0xFdcB2225A3D639004F60BDe28D48CF2403460b6e",
 } as const;
 
 export const SEPOLIA_CHAIN_ID = 11155111;
